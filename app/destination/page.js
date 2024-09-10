8da02a49d9b7b59b2860100b9eba5367fb03d2b7
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-09-10 23:04:25 +0200

    added api  and .env

--- app/destination/page.js
+++ app/destination/page.js
@@ -106,13 +106,7 @@ export const Destinations = () => {
           )}
           <b>List coming soon after lesson 3!</b>
 
-          {/* TASK - React 1 week 3 */}
-          {/* Import the AddWishlistItem react component */}
           <AddWishlistItem />
-          {/* TASK - React 1 week 3 */}
-          {/* Convert the list, so it is using selectedPlanets.map() to display the items  */}
-          {/* Implement the "REMOVE" function */}
-          {/* uncomment the following code snippet: */}
 
           <h3>Your current wishlist</h3>
           <div className={styles.wishlistList}>
