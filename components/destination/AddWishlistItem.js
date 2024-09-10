cbd466657b408c2ad5e16c3997e8e6bcb0cab84f
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-09-11 00:40:02 +0200

    navbar now is mapped

--- components/destination/AddWishlistItem.js
+++ components/destination/AddWishlistItem.js
@@ -10,10 +10,15 @@ export const AddWishlistItem = ({ onAddWishlistItem }) => {
 
   const [customWishliststate, onCustomWishliststate] = useState("");
   // TASK - React 1 week 3
+  console.log(customWishliststate);
   // 1. Add a useState for the handling the <input id="customWishlist" type="text" />
   // 2. Connect the onThumbnailChange to the <select>
 
   const onAddItemPressed = () => {
+    let inputValue = document.getElementById("customWishlist");
+
+    onAddWishlistItem(inputValue, thumbnail);
+    inputValue = "";
     // TASK - React 1 week 3
     // implement this function
     // Clear the <input/> field on button press
@@ -25,15 +30,22 @@ export const AddWishlistItem = ({ onAddWishlistItem }) => {
     <div className={styles.addWishlistItem}>
       <p>Add custom planet to wishlist</p>
       <label htmlFor="customWishlist">Wishlist item name</label>
-      <input id="customWishlist" type="text" />
+      <input
+        id="customWishlist"
+        type="text"
+        onChange={(e) => onCustomWishliststate(e.target.value)}
+      />
       <label htmlFor="customWishlistThumbnail">Wishlist item thumbnail</label>
-      <select id="customWishlistThumbnail">
+      <select
+        id="customWishlistThumbnail"
+        onChange={(e) => onThumbnailChange(e.target.value)}
+      >
         <option value="/destination/image-europa.png">EUROPA</option>
         <option value="/destination/image-mars.png">MARS</option>
         <option value="/destination/image-moon.png">MOON</option>
         <option value="/destination/image-titan.png">TITAN</option>
       </select>
-      <button>ADD CUSTOM</button>
+      <button onClick={() => onAddItemPressed()}>ADD CUSTOM</button>
     </div>
   );
 };
