0e87a3cec40c3ab93ad28c5dc00290a664bdc02b
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-09-10 21:10:30 +0200

    final commit

--- app/destination/page.js
+++ app/destination/page.js
@@ -41,6 +41,42 @@ export const Destinations = () => {
     );
   };
 
+  const arr = [
+    {
+      title: "Europa",
+      description:
+        "Europa, one of Jupiter’s moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers.",
+      thumbnail: "/destination/image-europa.png",
+      isSelected: isPlanetSelected["Europa"],
+      onAddOrRemovePlanet: () => onAddOrRemovePlanet("Europa", 1),
+    },
+    {
+      title: "Moon",
+      description:
+        "Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers.",
+
+      thumbnail: "/destination/image-moon.png",
+      isSelected: isPlanetSelected["Moon"],
+      onAddOrRemovePlanet: () => onAddOrRemovePlanet("Moon", 2),
+    },
+    {
+      title: "Mars",
+      description:
+        "Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity’s next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth.",
+      thumbnail: "/destination/image-mars.png",
+      isSelected: isPlanetSelected["Mars"],
+      onAddOrRemovePlanet: () => onAddOrRemovePlanet("Mars", 3),
+    },
+    {
+      title: "Titan",
+      description:
+        "Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets.",
+      thumbnail: "/destination/image-titan.png",
+      isSelected: isPlanetSelected["Titan"],
+      onAddOrRemovePlanet: () => onAddOrRemovePlanet("Titan", 4),
+    },
+  ];
+
   return (
     <div className="fullBGpicture">
       <main className="mainContent">
@@ -55,7 +91,6 @@ export const Destinations = () => {
           )}
           <b>List coming soon after lesson 3!</b>
 
-          {/* STOP! - this is for week 3!*/}
           {/* TASK - React 1 week 3 */}
           {/* Import the AddWishlistItem react component */}
           {/* <AddWishlistItem /> */}
@@ -78,47 +113,23 @@ export const Destinations = () => {
             />
           </div> */}
         </section>
+
         <section className="card">
           <h2>Possible destinations</h2>
-
-          <PlanetCard
-            name={"Europa"}
-            description={
-              "Europa, one of Jupiter’s moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers."
-            }
-            thumbnail={"/destination/image-europa.png"}
-            isSelected={isPlanetSelected["Europa"]}
-            onAddOrRemovePlanet={() => onAddOrRemovePlanet("Europa", 1)}
-          />
-          <PlanetCard
-            name={"Moon"}
-            description={
-              "Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers."
-            }
-            thumbnail={"/destination/image-moon.png"}
-            isSelected={isPlanetSelected["Moon"]}
-            onAddOrRemovePlanet={() => onAddOrRemovePlanet("Moon", 2)}
-          />
-
-          <PlanetCard
-            name={"Mars"}
-            description={
-              "Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity’s next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth."
-            }
-            thumbnail={"/destination/image-mars.png"}
-            isSelected={isPlanetSelected["Mars"]}
-            onAddOrRemovePlanet={() => onAddOrRemovePlanet("Mars", 3)}
-          />
-
-          <PlanetCard
-            name={"Titan"}
-            description={
-              "Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets."
-            }
-            thumbnail={"/destination/image-titan.png"}
-            isSelected={isPlanetSelected["Titan"]}
-            onAddOrRemovePlanet={() => onAddOrRemovePlanet("Titan", 4)}
-          />
+          {arr.map((value) => {
+            return (
+              <PlanetCard
+                name={value.title}
+                description={value.description}
+                thumbnail={value.thumbnail}
+                isSelected={value.isSelected}
+                onAddOrRemovePlanet={value.onAddOrRemovePlanet}
+              >
+                {" "}
+              </PlanetCard>
+            );
+          })}
+          // /> */}
         </section>
       </main>
     </div>
